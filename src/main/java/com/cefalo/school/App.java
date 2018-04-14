88362cccbabe91f29624daeb26c650e0f89e7cd2
JobaerChowdhury <jobaer.du@gmail.com>
2018-04-14 20:25:30 +0600

    Composite pattern example from DZone

--- src/main/java/com/cefalo/school/App.java
+++ src/main/java/com/cefalo/school/App.java
@@ -1,5 +1,9 @@
 package com.cefalo.school;
 
+import com.cefalo.school.composite.Developer;
+import com.cefalo.school.composite.Employee;
+import com.cefalo.school.composite.Manager;
+
 public class App {
 
     public static void main(String[] args) {
@@ -21,5 +25,19 @@ public class App {
         Developer emp5 = new Developer("Dev 4", "GB", 20000, devScore);
         Developer emp6 = new Developer("Dev 5", "GB", 30000, devScore);*/
 
+        test();
+    }
+
+    public static void test() {
+        Employee emp1=new Developer("John", 10000);
+        Employee emp2=new Developer("David", 15000);
+        Employee manager1=new Manager("Daniel",25000);
+        manager1.add(emp1);
+        manager1.add(emp2);
+        Employee emp3=new Developer("Michael", 20000);
+        Manager generalManager=new Manager("Mark", 50000);
+        generalManager.add(emp3);
+        generalManager.add(manager1);
+        generalManager.print();
     }
 }
