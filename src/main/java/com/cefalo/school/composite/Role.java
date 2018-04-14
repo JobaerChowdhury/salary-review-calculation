38560d156e72ed0e8e9138f97db291e945f789fe
JobaerChowdhury <jobaer.du@gmail.com>
2018-04-14 22:40:55 +0600

    Include the impact inside the role

--- src/main/java/com/cefalo/school/composite/Role.java
+++ src/main/java/com/cefalo/school/composite/Role.java
@@ -1,8 +1,20 @@
 package com.cefalo.school.composite;
 
+import com.cefalo.school.calculator.Impact;
+
 public enum Role {
-    DEVELOPER,
-    TEAMLEAD,
-    PROJECTMANAGER,
-    CTO
+    DEVELOPER(new Impact(0.2, 0.2, 0.2, 0.2, 0.2)),
+    TEAMLEAD(new Impact(0.3, 0.1, 0.3, 0.3, 0.0)),
+    PROJECTMANAGER(new Impact(0.2, 0.1, 0.3, 0.4, 0.1)),
+    CTO(new Impact(0.1, 0.3, 0.3, 0.3, 0.0));
+
+    private Impact impact;
+
+    private Role(Impact impact) {
+        this.impact = impact;
+    }
+
+    public Impact getImpact() {
+        return this.impact;
+    }
 }
