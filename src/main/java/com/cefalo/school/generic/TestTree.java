1a9cbb4ed1f94f8118fbc57a306e894ffb7690f1
JobaerChowdhury <jobaer.du@gmail.com>
2018-04-17 23:51:58 +0600

    Handle salary increment, both with impact/score, and flat increment

--- src/main/java/com/cefalo/school/generic/TestTree.java
+++ src/main/java/com/cefalo/school/generic/TestTree.java
@@ -1,9 +1,6 @@
 package com.cefalo.school.generic;
 
-import com.cefalo.school.calculator.EmployeeInfo;
-import com.cefalo.school.calculator.EmployeeInfoImpl;
-import com.cefalo.school.calculator.Role;
-import com.cefalo.school.calculator.Score;
+import com.cefalo.school.calculator.*;
 
 public class TestTree {
     public static void main(String[] args) {
@@ -13,7 +10,7 @@ public class TestTree {
 
         EmployeeInfo cto = tree.createNode(new EmployeeInfoImpl(1, "john the cto", Role.CTO, 100000, goodScore));
 
-        EmployeeInfo pmJane = tree.createNode(new EmployeeInfoImpl(2, "jane the pm", Role.PROJECTMANAGER, 90000, goodScore));
+        EmployeeInfo pmJane = tree.createNode(new EmployeeInfoImpl(2, "jane the pm", Role.PROJECTMANAGER, 80000, goodScore));
         EmployeeInfo tlTim = tree.createNode(new EmployeeInfoImpl(4, "tim the tl", Role.TEAMLEAD, 70000, badScore));
         EmployeeInfo devDon = tree.createNode(new EmployeeInfoImpl(7, "don the dev", Role.DEVELOPER, 30000, goodScore));
         EmployeeInfo devDime = tree.createNode(new EmployeeInfoImpl(8, "dime the dev", Role.DEVELOPER, 40000, goodScore));
@@ -45,14 +42,32 @@ public class TestTree {
 //        tree.forEach(cto, System.out::println);
 
         // Find the sum of everyones salary using fold
-        Double result = tree.fold(pmJane,0.0, e -> e.getSalary(), (a, b) -> a + b);
-        Double result2 = tree.fold(pmJim,0.0, e -> e.getSalary(), (a, b) -> a + b);
-        Double result3 = tree.fold(cto,0.0, e -> e.getSalary(), (a, b) -> a + b);
+        Double result = tree.fold(pmJane, 0.0, e -> e.getSalary(), (a, b) -> a + b);
+        Double result2 = tree.fold(pmJim, 0.0, e -> e.getSalary(), (a, b) -> a + b);
+        Double result3 = tree.fold(cto, 0.0, e -> e.getSalary(), (a, b) -> a + b);
         System.out.println("result = " + result);
         System.out.println("result2 = " + result2);
         System.out.println("result3 = " + result3);
 
+        Double newSalary = newSalary(cto);
+        System.out.printf("newSalary cto = %.2f\n", newSalary);
+
+        Double afterIncreaseTotal = tree.fold(cto, 0.0, e -> newSalary(e), (a, b) -> a + b);
+        System.out.printf("afterIncreaseTotal = %.2f\n", afterIncreaseTotal);
+
+        Double flatIncreaseTotal = tree.fold(cto, 0.0, e -> flatIncrease(e, 0.05), (a, b) -> a + b);
+        System.out.printf("flatIncreaseTotal = %.2f\n", flatIncreaseTotal);
+
         System.out.println("END");
+    }
+
+    private static Double flatIncrease(EmployeeInfo emp, Double percentage) {
+        return emp.getSalary() * (1 + percentage);
+    }
+
+    private static Double newSalary(EmployeeInfo emp) {
+        ReviewCalculator reviewCalculator = new ReviewCalculator(emp.getSalary(), emp.getScore(), emp.getRole().getImpact());
+        return reviewCalculator.calculate();
     }
 
     private static Score goodScore = new Score(8, 5, 9, 8, 9);
